ffb5ec5a8f45eaaa5afc93155e9def6584040e49
yuto343 <namawasabi1229@gmail.com>
2024-08-21 05:33:49 +0900

    作成

---
+++ src/components/scroll-done.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from "svelte";
+
+  let count = 0;
+
+  function countUp() {
+    count += 1;
+  }
+
+  onMount(() => {
+    const interval = setInterval(countUp, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div>
+  Count：{count}
+</div>
+
+<style>
+  div {
+    font-size: 30px;
+    font-weight: bold;
+  }
+</style>
